862ef78e2c2c4347a6ccbe447384c40aff98a688
skyvault05 <kimzombie@hotmail.com>
2022-07-07 02:50:30 +0900

    Add Redis Driver, Add RedisTest
    * Add Initial Setting of Redis
    * Add RedisTest and Connection tested succesfully

--- src/main/java/com/skyvault05/remindme/config/jpa/JpaConfig.java
+++ src/main/java/com/skyvault05/remindme/config/jpa/JpaConfig.java
@@ -1,4 +1,4 @@
-package com.skyvault05.remindme.config;
+package com.skyvault05.remindme.config.jpa;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
---
+++ src/test/java/com/skyvault05/remindme/domain/RedisTest.java
@@ -0,0 +1,32 @@
+package com.skyvault05.remindme.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import static org.junit.Assert.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class RedisTest {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void redisConnectionTest() {
+        String key = "a";
+        String data = "1";
+
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(key, data);
+
+        String result = valueOperations.get(key);
+        assertEquals(data, result);
+    }
+}
