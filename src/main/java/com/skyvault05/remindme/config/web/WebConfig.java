9320eb84acd5a6be6157c6dfce62e49b6d22c3ae
skyvault05 <kimzombie@hotmail.com>
2023-01-25 06:55:45 +0900

    Test CORS

--- src/main/java/com/skyvault05/remindme/config/web/WebConfig.java
+++ src/main/java/com/skyvault05/remindme/config/web/WebConfig.java
@@ -1,18 +1,18 @@
-//package com.skyvault05.remindme.config.web;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//@EnableWebMvc
-//public class WebConfig implements WebMvcConfigurer {
-//    @Value("${frontEndUrl}")
-//    private String frontEndUrl;
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**").allowedOrigins(frontEndUrl);
-//    }
-//}
+package com.skyvault05.remindme.config.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${frontEndUrl}")
+    private String frontEndUrl;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins(frontEndUrl);
+    }
+}
